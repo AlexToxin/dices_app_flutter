02236a63b410aa841f5fc5ca92db33a4415b7c1a
AlexToxin <avesuslov@gmail.com>
2020-05-21 16:09:06 +0400

    Change classes to constants

---
+++ lib/dices/genesys/genesys_types.dart
@@ -0,0 +1,8 @@
+class GenesysTypes {
+  static const Advantage = 'Advantage';
+  static const Despair = 'Despair';
+  static const Failure = 'Failure';
+  static const Success = 'Success';
+  static const Threat = 'Threat';
+  static const Triumph = 'Triumph';
+}
